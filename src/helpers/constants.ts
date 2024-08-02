6c560d9cddc35d3c47ee82a97a882f70fa29aea2
Maxim_belish <maxamax997@gmail.com>
2024-08-02 18:23:15 +0300

    edit delayTime

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,6 +1,6 @@
 export const inactivityTimeout: number = 3000; // Время задержки менюшки с действиями (add, edit,del)
 
-export const delayTime: number = 0;  // Время задержки генерации столбцов и строк (симулятор ответа от сервера)
+export const delayTime: number = 1500;  // Время задержки генерации столбцов и строк (симулятор ответа от сервера)
 
 export const validNumber: { min: number; max: number } = {
    min: 2, // Минимальное количество столбцов и строк
